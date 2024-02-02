3e06f385223cfd488ca505ac752f1007fbb08a00
Leonardo Ramos <lmassalairamos@gmail.com>
2024-02-02 17:22:42 -0300

    style: basic animation

--- src/components/Pokemons/index.tsx
+++ src/components/Pokemons/index.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { formatName } from '../../lib'
@@ -21,21 +22,25 @@ export const Pokemons = () => {
   const pokemons = useSelector(getPokemons)
 
   return (
-    <ul
+    <motion.ul
+      layout
+      layoutRoot
       role='list'
       className='grid grid-cols-1 sm:grid-cols-2 gap-6 lg:grid-cols-4'
     >
-      {pokemons.slice(0, 20).map((pokemon, index) => (
-        <PokemonCard key={pokemon.name + index} {...pokemon} />
-      ))}
-    </ul>
+      <AnimatePresence mode='popLayout'>
+        {pokemons.slice(0, 20).map((pokemon, index) => (
+          <PokemonCard key={pokemon.name + index} {...pokemon} />
+        ))}
+      </AnimatePresence>
+    </motion.ul>
   )
 }
 
 export const PokemonCard = ({ name }: IPokemon) => {
   return (
-    <li
-      key={name}
+    <motion.li
+      layout
       className='group relative overflow-hidden col-span-1 rounded-lg bg-white shadow cursor-pointer p-6'
     >
       <div className='absolute inset-0 bg-gradient-to-r from-zinc-800 to-gray-950 translate-y-[100%] group-hover:translate-y-[0%] transition-transform duration-300' />
@@ -43,6 +48,6 @@ export const PokemonCard = ({ name }: IPokemon) => {
       <h3 className='truncate text-lg font-medium relative z-10 text-gray-900 group-hover:text-white transition-all duration-300'>
         {formatName(name)}
       </h3>
-    </li>
+    </motion.li>
   )
 }
