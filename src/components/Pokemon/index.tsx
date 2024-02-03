184a86659c89772079e457b6a4d4aa3016f19a3f
Leonardo Ramos <lmassalairamos@gmail.com>
2024-02-03 17:13:17 -0300

    refactor: removing commented code

--- src/components/Pokemon/index.tsx
+++ src/components/Pokemon/index.tsx
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react'
-import { EnvelopeIcon, PhoneIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { XMarkIcon } from '@heroicons/react/24/outline'
 import { Fragment, useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { cn, fetchData, formatName } from '../../lib'
@@ -153,34 +153,6 @@ export const Pokemon = () => {
                     ))}
                   </dl>
                 </div>
-                {/* <div>
-                  <div className='-mt-px flex divide-x divide-gray-200'>
-                    <div className='flex w-0 flex-1'>
-                      <a
-                        href={`#`}
-                        className='relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900'
-                      >
-                        <EnvelopeIcon
-                          className='h-5 w-5 text-gray-400'
-                          aria-hidden='true'
-                        />
-                        Email
-                      </a>
-                    </div>
-                    <div className='-ml-px flex w-0 flex-1'>
-                      <a
-                        href={`#`}
-                        className='relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900'
-                      >
-                        <PhoneIcon
-                          className='h-5 w-5 text-gray-400'
-                          aria-hidden='true'
-                        />
-                        Call
-                      </a>
-                    </div>
-                  </div>
-                </div> */}
               </Dialog.Panel>
             </Transition.Child>
           </div>
